e836baa3ccca71560c14b0247589ad6d749e629d
Trần Văn Tài <ngoctai.dev@gmail.com>
2019-04-20 00:23:40 +0700

    Upload layout index page but not compelete.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,76 @@
 <!DOCTYPE html>
 <html lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Title Page</title>
 
-        <!-- Bootstrap CSS -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title Page</title>
+    <base href="{{ asset('') }}">
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-        <h1 class="text-center">Hello World</h1>
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/custom/index.css">
 
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Bootstrap JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </body>
+</head>
+
+<body>
+
+    <header>
+        <div class="container-fruid">
+            <div class="row">
+                <div class="col-sm-3 text-center">
+                    <img src="public/images/header-logo.jpg" class="img-circle" width="198px" alt="banner">
+                </div>
+                <div class="col-sm-9 text-center">
+                    <h2>Chao mung ban den voi ngoi nha PNV</h2>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container-fruid">
+
+            <div class="jumbotron">
+                <div class="container text-center">
+                    <h2>Passerelles Numeriques Vietnam</h2>
+                    <p>A gateway for life</p>
+                    <p>
+                        <a class="btn btn-primary btn-lg">Login</a>
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3">
+                    Menu Doc
+                </div>
+                <div class="col-sm-6">
+                    Content
+                </div>
+                <div class="col-sm-3">
+                    Side Bar
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container-fruid">
+            <div class="row">
+                <div class="col-sm-3">1</div>
+                <div class="col-sm-3">2</div>
+                <div class="col-sm-3">3</div>
+                <div class="col-sm-3">4</div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
 </html>
