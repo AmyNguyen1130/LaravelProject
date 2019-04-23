54c6307ef2c01a3880d937f3513fec36f5bf4f6e
Tran Van Tai <tai.tran@pn.com.vn>
2019-04-23 18:08:07 +0700

    WIP - Update user interface

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,9 +18,34 @@
 
     <header>
         <div class="container-fruid">
-            <div class="row">
+            
+            <nav class="navbar navbar-default" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="">DORMITORY</a>
+                </div>
+            
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li>KÍ TÚC XÁ LÀ NHÀ - BẠN BÈ LÀ ANH EM</li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Đăng nhập</a></li>
+                        <li><a href="#">Đăng ký</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+            
+            <!-- <div class="row">
                 <div class="col-sm-3 text-center">
-                    <a href=""><img src="public/images/header-logo.jpg" class="img-circle" width="150px" alt="banner"></a>
+                    <a href=""><img src="public/images/header-logo.jpg" class="img-circle" width="100px" alt="banner"></a>
                 </div>
                 <div class="col-sm-6" id="sologan">
                     <h3>KÍ TÚC XÁ LÀ NHÀ - BẠN BÈ LÀ ANH EM</h3>
@@ -29,7 +54,7 @@
                     <button class="btn btn-primary">Đăng nhập</button>
                     <button class="btn btn-default">Đăng ký</button>
                 </div>
-            </div>
+            </div> -->
         </div>
     </header>
 
